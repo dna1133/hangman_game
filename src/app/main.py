3ee7ed464e56614f4bbff3978d9670d32b01eb55
Alex Kor <dna1133@gmail.com>
2024-10-24 08:23:55 +0300

    init: Initial commit

---
+++ src/app/main.py
@@ -0,0 +1,11 @@
+def start_app() -> None:
+    """
+    game = Game()
+    ui = UI()
+    presenter = Presenter(game, ui)
+    presenter.run()
+    """
+
+
+if __name__ == "__main__":
+    start_app()
